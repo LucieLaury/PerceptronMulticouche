4c6dee92de9678155001f7b20252de13b232f177
Yann Mijatovic <yannmijatovicp@gmail.com>
2023-12-08 16:48:28 +0100

    TanH + pseudo-code

---
+++ src/mlp/TangenteHyperbolique.java
@@ -0,0 +1,13 @@
+package mlp;
+
+public class TangenteHyperbolique implements TransferFunction{
+    @Override
+    public double evaluate(double value) {
+        return Math.tanh(value);
+    }
+
+    @Override
+    public double evaluateDer(double value) {
+        return 1 - evaluate(value);
+    }
+}
