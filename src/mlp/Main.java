46adbeca4f95992f15748321f449ff8148d26ff9
Lucie <lucye.laury@gmail.com>
2023-12-08 16:40:05 +0100

    table ET OU XOR

--- src/mlp/Main.java
+++ src/mlp/Main.java
@@ -2,6 +2,10 @@ package mlp;
 
 public class Main {
     public static void main(String[] args) {
+        double[][] tableET = {{-1., 1., -1.}, {-1., -1., -1.}, {1., -1., -1.}, {1., 1., 1.}};
+        double[][] tableOU = {{-1., -1., -1.}, {-1., 1., 1.}, {1., -1., 1.}, {1., 1., 1.}};
+        double[][] tableXOR = {{-1, -1., -1.}, {-1., 1., 1.}, {1., -1., 1.}, {1., 1., 1.}};
+
 
     }
 }
